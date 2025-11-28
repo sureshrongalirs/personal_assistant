8018ca3f4ac99e9a1ce099d2efa85e619c534bad
sureshrongalirs <suresh.rongalirs@gmail.com>
2025-11-29 00:42:52 +0530

    folder structure added

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # personal_assistant
+
+# How to run?
+### Steps:
+
+Clone the repository
+
+'''bash
+https://github.com
+
+'''bash
+
+### Step 01- Create a conda environment after opening the repository
+
+'''bash
+conda create -n agentapp python=3.12 -y
+
+'''bash
+conda activate agentapp
+
+### Step 02- Install the requirements
+'''bash
+pip install -r requirements.txt
+'''
+
+Now,
+'''bash
+#Start the flask server
+python app.py
+'''
+
---
+++ template.py
@@ -0,0 +1,36 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s]: %(message)s')
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    "backend/ingest_data.py",
+    "backend/model.py",
+    "static/app.js",
+    "static/styles.css",
+    "app.py",
+    ".env",
+    "Dockerfile",
+    "requirements.txt",
+    "templates/index.html"
+    
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Created directory: {filedir} for file: {filename}")
+        
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+        logging.info(f"Created empty file: {filepath}")
+        
+    else:
+        logging.info(f"{filename} is already exists")
+                
